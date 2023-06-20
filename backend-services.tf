dae176e877beabc5e3585428e1bf2889fd47a250
Bimanboro <bimanboro8437@gmail.com>
2023-06-20 12:49:41 +0530

    Deploy

--- backend-services.tf
+++ backend-services.tf
@@ -20,7 +20,7 @@ resource "aws_db_instance" "vprofile-rds" {
   instance_class         = "db.t2.micro"
   username               = var.dbuser
   password               = var.dbpass
-  parameter_group_name   = "default.mysql5.6"
+  parameter_group_name   = "default.mysql8.0"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
