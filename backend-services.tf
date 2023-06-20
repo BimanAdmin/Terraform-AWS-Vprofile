241c6417ed8fcdedfe628a98ef637fd5a4e39537
Bimanboro <bimanboro8437@gmail.com>
2023-06-20 11:48:12 +0530

    backend

--- backend-services.tf
+++ backend-services.tf
@@ -1,13 +1,15 @@
-resource "aws_db_subnet_group" "vprofile-rds-subgroup" {
+resource "aws_db_subnet_group" "vprofile-rds-subgrp" {
   name       = "main"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
   tags = {
-    name = "Subnet groups for RDS"
+    Name = "Subnet group for RDS"
   }
 }
+
 resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
   name       = "vprofile-ecache-subgrp"
   subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]]
+
 }
 
 resource "aws_db_instance" "vprofile-rds" {
@@ -16,13 +18,14 @@ resource "aws_db_instance" "vprofile-rds" {
   engine                 = "mysql"
   engine_version         = "5.6.34"
   instance_class         = "db.t2.micro"
+  name                   = var.dbname
   username               = var.dbuser
   password               = var.dbpass
   parameter_group_name   = "default.mysql5.6"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.vprofile-rds-subgroup.name
+  db_subnet_group_name   = aws_db_subnet_group.vprofile-rds-subgrp.name
   vpc_security_group_ids = [aws_security_group.vprofile-backend-sg.id]
 }
 
