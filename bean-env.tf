1403c00071efa115d9a478493e4d5f9ff8de0037
Bimanboro <bimanboro8437@gmail.com>
2023-06-21 00:47:44 +0530

    Deploy

--- bean-env.tf
+++ bean-env.tf
@@ -7,7 +7,7 @@ resource "aws_elastic_beanstalk_application" "vprofile-prod" {
 }
 
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
-  name                = "vprofile-bean-prod"
+
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
   solution_stack_name = "64bit Amazon Linux 2 v4.3.8 running Tomcat 8.5 Corretto 11"
 
